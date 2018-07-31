c4cd656b4229f5f7b73f8d8519909863c694a289
midimurphdesigns <midimurphdesigns@gmail.com>
2018-07-31 16:11:23 -0700

    added function to handle when all cards have been swiped

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View, Animated } from "react-native";
-import { Card, Button } from 'react-native-elements';
+import { Card, Button } from "react-native-elements";
 
 import Deck from "./src/Deck";
 
@@ -48,32 +48,37 @@ const DATA = [
 ];
 
 export default class App extends React.Component {
-
   renderCard(item) {
     return (
-      <Card
-        key={item.id}
-        title={item.text}
-        image={{ uri: item.uri }}
-      >
+      <Card key={item.id} title={item.text} image={{ uri: item.uri }}>
         <Text style={{ marginBottom: 10 }}>
           I can customize the Card further.
         </Text>
-        <Button 
-          icon={{ name: 'code' }}
+        <Button
+          icon={{ name: "code" }}
           backgroundColor="#03A9F4"
           title="View Now!"
         />
       </Card>
-    )
+    );
+  }
+
+  renderNoMoreCards() {
+    return (
+      <Card title="All Done!">
+        <Text style={{ marginBottom: 10 }}>There's no more content here!</Text>
+        <Button backgroundColor="#03A9F4" title="Get more!" />
+      </Card>
+    );
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Deck 
+        <Deck
           data={DATA}
           renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
         />
       </View>
     );
