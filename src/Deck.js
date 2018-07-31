e9077a14ada687c19d9f4545d85f7b0d4574e700
midimurphdesigns <midimurphdesigns@gmail.com>
2018-07-31 15:35:32 -0700

    card leaves screen when swiped left and right

--- src/Deck.js
+++ src/Deck.js
@@ -10,6 +10,7 @@ import {
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 export default class Deck extends Component {
   constructor(props) {
@@ -23,9 +24,9 @@ export default class Deck extends Component {
       },
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > SWIPE_THRESHOLD) {
-          console.log("swipe right!");
+          this.forceSwipe('right');
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
-          console.log("swipe left!");
+          this.forceSwipe('left');
         } else {
           this.resetPosition();
         }
@@ -33,6 +34,14 @@ export default class Deck extends Component {
     });
 
     this.state = { panResponder, position };
+  }
+
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+    Animated.timing(this.state.position, {
+      toValue: { x, y: 0},
+      duration: SWIPE_OUT_DURATION
+    }).start();
   }
 
   resetPosition() {
